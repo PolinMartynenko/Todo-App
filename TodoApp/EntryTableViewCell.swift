c70499328432edae8dc5eb18eaa2d96bc69570a6
Polina Martynenko <>
2022-04-21 22:17:16 +0300

    add handleSwitchChange

---
+++ TodoApp/EntryTableViewCell.swift
@@ -0,0 +1,89 @@
+//
+//  EntryTableViewCell.swift
+//  TodoApp
+//
+//  Created by Polina Martynenko on 19.04.2022.
+//
+
+import UIKit
+
+class EntryTableViewCell : UITableViewCell {
+    let listLabel = UILabel()
+    let editButton = UIButton()
+    let uiSwitch = UISwitch()
+    let stackView = UIStackView()
+    var handleSwitchChange: ((Bool) -> Void)?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .cyan
+        setUpStackView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func swichValueChanged(_ uiSwitch: UISwitch){
+        print(uiSwitch.isOn)
+        handleSwitchChange?(uiSwitch.isOn)
+//        entry.isCompleted = uiSwitch.isOn
+    }
+    
+    @objc func editButtonTouched(){
+        print("Touched")
+    }
+    
+    
+    
+    private func setUpStackView(){
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        
+        contentView.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            
+        ])
+        
+        setUpSwitch()
+        setUpLable()
+        setUpEditButton()
+    }
+    
+    private func setUpSwitch (){
+        uiSwitch.addTarget(self, action: #selector(swichValueChanged), for: .valueChanged)
+//        uiSwitch.isOn = entry.isCompleted
+        stackView.addArrangedSubview(uiSwitch)
+        uiSwitch.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setUpLable(){
+//        listLabel.text = entry.text
+        listLabel.numberOfLines = 0
+        stackView.addArrangedSubview(listLabel)
+        listLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    private func setUpEditButton(){
+        editButton.setTitle("Edit", for: .normal)
+        editButton.setTitleColor(.red, for: .normal)
+        editButton.backgroundColor = .blue //цвет фотки
+        stackView.addArrangedSubview(editButton)
+        editButton.layer.cornerRadius = 10 //скругление кнопки
+        editButton.addTarget(self, action: #selector(editButtonTouched), for: .touchUpInside)
+        editButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            editButton.widthAnchor.constraint(equalToConstant: 60)
+        ])
+        
+    }
+    
+    
+}
+
--- TodoApp/TodoListViewController.swift
+++ TodoApp/TodoListViewController.swift
@@ -9,10 +9,7 @@ import UIKit
 
 class TodoListViewController: UIViewController {
     let addButton = UIButton()
-    let listLabel = UILabel()
-    let editButton = UIButton()
-    let uiSwitch = UISwitch()
-    let stackView = UIStackView()
+    
     let tableView = UITableView()
     
     var entries = [Entry](){ //пустой массив
@@ -43,39 +40,12 @@ class TodoListViewController: UIViewController {
         
     }
     
-    @objc func swichValueChanged(_ uiSwitch: UISwitch){
-        print(uiSwitch.isOn)
-//        entry.isCompleted = uiSwitch.isOn
-    }
     
-    @objc func editButtonTouched(){
-        print("Touched")
-        let alert = UIAlertController(title: "Edit", message: nil, preferredStyle: .alert)
-        
-        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
-            print("Ok taped")
-            let textField = alert.textFields?.first
-            let text = textField?.text
-            print(text)
-            self.listLabel.text = text
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
-            print("Cancel")
-        }
-        alert.addTextField { textField in
-            textField.placeholder = "Your task..."
-            textField.text = self.listLabel.text
-        }
-        
-        alert.addAction(okAction)
-        alert.addAction(cancelAction)
-        self.present(alert, animated: true, completion: nil)
-    }
     
     private func setUpTableView(){
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.register(EntryTableViewCell.self, forCellReuseIdentifier: "Cell")
         self.view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -90,53 +60,7 @@ class TodoListViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTouched))
     }
     
-    private func setUpStackView(){
-        stackView.axis = .horizontal
-        stackView.spacing = 10
-        
-        view.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            
-        ])
-        
-        setUpSwitch()
-        setUpLable()
-        setUpEditButton()
-    }
-    
-    private func setUpSwitch (){
-        uiSwitch.addTarget(self, action: #selector(swichValueChanged), for: .valueChanged)
-//        uiSwitch.isOn = entry.isCompleted
-        stackView.addArrangedSubview(uiSwitch)
-        uiSwitch.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    private func setUpLable(){
-//        listLabel.text = entry.text
-        listLabel.numberOfLines = 0
-        stackView.addArrangedSubview(listLabel)
-        listLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-    }
-    
-    private func setUpEditButton(){
-        editButton.setTitle("Edit", for: .normal)
-        editButton.setTitleColor(.red, for: .normal)
-        editButton.backgroundColor = .blue //цвет фотки
-        stackView.addArrangedSubview(editButton)
-        editButton.layer.cornerRadius = 10 //скругление кнопки
-        editButton.addTarget(self, action: #selector(editButtonTouched), for: .touchUpInside)
-        editButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            editButton.widthAnchor.constraint(equalToConstant: 60)
-        ])
-        
-    }
+   
 }
 
 
@@ -154,11 +78,19 @@ extension TodoListViewController: AddToDoDelegate {
 extension TodoListViewController: UITableViewDataSource { // создание ячеек таблицы(источник данных)
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //ячейки для таблицы мы не инициализируем на прямую, а используем этот метод
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let entry =  entries[indexPath.row]
-        cell.textLabel?.text = entry.text
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? EntryTableViewCell {
+            var entry =  entries[indexPath.row]
+            cell.listLabel.text = entry.text
+            cell.uiSwitch.isOn = entry.isCompleted
+            cell.handleSwitchChange = { isOn in
+                entry.isCompleted = cell.uiSwitch.isOn
+                self.entries[indexPath.row] = entry
+            }
+            return cell
+        } else {
+            return tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        }
         
-        return cell
     }
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
